f1b61d8e224cd8e2246677a84eaa1d72e0fc0504
henry hoo <hejiaxi@bupt.edu.cn>
2016-04-08 07:00:59 -0700

    first version of listen to self

--- hw5.cpp
+++ hw5.cpp
@@ -13,7 +13,7 @@ const std::size_t bufsize = 8192;
 
 int main(int argc, char *argv[]){
 	int pid = atoi(argv[1]);
-	int c1=0,c2=0;
+	int where;
 	double t = atof(argv[2]);
 	double child1,child2,parent,now;
 	mqd_t mq1,mq2,mqp,mqself;
@@ -38,187 +38,138 @@ int main(int argc, char *argv[]){
 		switch(pid){
 			case 0:
 				now = t;
-				mq1 = mq_open("/71",O_RDONLY | O_CREAT | O_NONBLOCK,0666,NULL);
+				mq1 = mq_open("/71",O_WRONLY | O_CREAT | O_NONBLOCK,0666,NULL);
 				if(mq1 == -1){
 					perror("mq_open()");
 				}
-				mq2 = mq_open("/72",O_RDONLY | O_CREAT | O_NONBLOCK,0666,NULL);
+				mq2 = mq_open("/72",O_WRONLY | O_CREAT | O_NONBLOCK,0666,NULL);
 				if(mq2 == -1){
 					perror("mq_open()");
 				}
-				mqself = mq_open("/70",O_WRONLY | O_CREAT | O_NONBLOCK,0666,NULL);
+				mqself = mq_open("/70",O_RDONLY | O_CREAT | O_NONBLOCK,0666,NULL);
 				if(mqself == -1){
 					perror("mq_open()");
 				}
-				sprintf(buffer,"%f",now);
+				//0 for from parents,1 for child1,2 for child 2
+				sprintf(buffer,"%d%f",0,now);
 				cout<<"process "<<pid<<": current temperature"<<now<<endl;
-				if (mq_send(mqself, buffer, strlen(buffer), 0) == -1) {
+				//begin the circue
+				if (mq_send(mq1, buffer, strlen(buffer), 0) == -1) {
 					perror("mq_send()");
 				}
-				if (mq_send(mqself, buffer, strlen(buffer), 0) == -1) {
+				if (mq_send(mq2, buffer, strlen(buffer), 0) == -1) {
 					perror("mq_send()");
 				}
-
 				while(true){
-					if (mq_receive(mq1, buffer, bufsize, NULL) == -1) {
+					if (mq_receive(mqself, buffer, bufsize, NULL) == -1) {
 						if (errno == EAGAIN) {
-							//							cout << "No message 1\n";
+//							std::cout << "No message\n";
 						} else {
 							perror("mq_receive()");
 						}
 					} else {
-						//						cout << "Received message 1: `" << buffer << "'.\n";
-						child1 = atof(buffer);
-						c1++;
-
-					}
-
-					if (mq_receive(mq2, buffer, bufsize, NULL) == -1) {
-						if (errno == EAGAIN) {
-							//						cout << "No message 2\n"<<now;
-						} else {
-							perror("mq_receive()");
+						where=buffer[0]-48;
+						strncpy(buffer, buffer+1, bufsize-1);
+						if(where==0){
+							parent=atof(buffer);
 						}
-					} else {
-						//				cout << "Received message 2: `" << buffer << "'.\n";
-						child2 = atof(buffer);
-						c2++;
-						while(c1<c2){
-
-							if (mq_receive(mq1, buffer, bufsize, NULL) == -1) {
-								if (errno == EAGAIN) {
-									//							cout << "No message 1\n";
-								} else {
-									perror("mq_receive()");
-								}
-							} else {
-								//						cout << "Received message 1: `" << buffer << "'.\n";
-								child1 = atof(buffer);
-								c1++;
-
-							}
+						if(where==1){
+							child1=atof(buffer);
 						}
-						if(fabs(now-(now+child1+child2)/3)<0.01){
+						if(where==2){
+							child2=atof(buffer);
+							if(fabs(now-(child1+child2+now)/3)<0.01){
 							cout<<"process "<<pid<<": final temperature"<<now<<endl;
 							exit(0);
-						}
-						else{
-							now=(now+child1+child2)/3;
-							sprintf(buffer,"%f",now);
+}else{
+							now=(child1+child2+now)/3;	
+							sprintf(buffer,"%d%f",0,now);
 							cout<<"process "<<pid<<": current temperature"<<now<<endl;
-							if (mq_send(mqself, buffer, strlen(buffer), 0) == -1) {
+							if (mq_send(mq1, buffer, strlen(buffer), 0) == -1) {
 								perror("mq_send()");
 							}
-
-							if (mq_send(mqself, buffer, strlen(buffer), 0) == -1) {
+							if (mq_send(mq2, buffer, strlen(buffer), 0) == -1) {
 								perror("mq_send()");
 							}
-						}
+						}}											
 					}
 				}
 				break;
 			case 1:	
-				mqp = mq_open("/70",O_RDONLY | O_CREAT | O_NONBLOCK,0666,NULL);
+				mqp = mq_open("/70",O_WRONLY | O_CREAT | O_NONBLOCK,0666,NULL);
 				if(mqp == -1){
 					perror("mq_open()");
 				}	
-				mq1 = mq_open("/73",O_RDONLY | O_CREAT | O_NONBLOCK,0666,NULL);
+				mq1 = mq_open("/73",O_WRONLY | O_CREAT | O_NONBLOCK,0666,NULL);
 				if(mq1 == -1){
 					perror("mq_open()");
 				}
-				mq2 = mq_open("/74",O_RDONLY | O_CREAT | O_NONBLOCK,0666,NULL);
+				mq2 = mq_open("/74",O_WRONLY | O_CREAT | O_NONBLOCK,0666,NULL);
 				if(mq2 == -1){
 					perror("mq_open()");
 				}
-				mqself = mq_open("/71",O_WRONLY | O_CREAT | O_NONBLOCK,0666,NULL);
+				mqself = mq_open("/71",O_RDONLY | O_CREAT | O_NONBLOCK,0666,NULL);
 				if(mq2 == -1){
 					perror("mq_open()");
 				}
 				now=t;
-				sprintf(buffer,"%f",now);
+
 				//cout<<"process "<<pid<<": current temperature"<<now<<endl;	
 				while(true){
-					if (mq_receive(mq1, buffer, bufsize, NULL) == -1) {
+					if (mq_receive(mqself, buffer, bufsize, NULL) == -1) {
 						if (errno == EAGAIN) {
-							//cout << "No message 1\n";
+//							std::cout << "No message\n";
 						} else {
 							perror("mq_receive()");
 						}
 					} else {
-						//cout << "Received message 1: `" << buffer << "'.\n";
-						child1 = atof(buffer);
-c1++;
+						where=(int)buffer[0]-48;
+						cout<<where<<endl;
+						strncpy(buffer, buffer+1, bufsize-1);
+						cout<<atof(buffer)<<endl;
+						if(where==0){
+							parent=atof(buffer);
+							now=(parent+now)/2;
+							sprintf(buffer,"%d%f",0,now);
+							cout<<"process "<<pid<<": current temperature"<<now<<endl;
+							if (mq_send(mq1, buffer, strlen(buffer), 0) == -1) {
+								perror("mq_send()");
+							}
+							if (mq_send(mq2, buffer, strlen(buffer), 0) == -1) {
+								perror("mq_send()");
+							}}
+						if(where==1){
+							child1=atof(buffer);
+						}
+						if(where==2){
+							child2=atof(buffer);
+							now=(child1+child2+now)/3;	
+							sprintf(buffer,"%d%f",1,now);
+							cout<<"process "<<pid<<": current temperature"<<now<<endl;
+							if (mq_send(mqp, buffer, strlen(buffer), 0) == -1) {
+								perror("mq_send()");
+							}
+
+						}											
 					}
 
-					if (mq_receive(mq2, buffer, bufsize, NULL) == -1) {
-						if (errno == EAGAIN) {
-							//cout << "No message 2\n"<<now;
-						} else {
-							perror("mq_receive()");
-						}
-					} else {
-						//cout << "Received message 2: `" << buffer << "'.\n";
-						child2 = atof(buffer);
-c2++;
-while(c1<c2){
 
-					if (mq_receive(mq1, buffer, bufsize, NULL) == -1) {
-						if (errno == EAGAIN) {
-							//							cout << "No message 1\n";
-						} else {
-							perror("mq_receive()");
-						}
-					} else {
-						//						cout << "Received message 1: `" << buffer << "'.\n";
-						child1 = atof(buffer);
-						c1++;
-
-					}
-}
-						if(child1==child1) sleep(0.5);
-						now=(now+child1+child2)/3;
-						sprintf(buffer,"%f",now);
-						cout<<"process "<<pid<<": current temperature"<<now<<endl;
-						if (mq_send(mqself, buffer, strlen(buffer), 0) == -1) {
-							perror("mq_send()");
-						}
-					}
-					if (mq_receive(mqp, buffer, bufsize, NULL) == -1) {
-						if (errno == EAGAIN) {
-							//cout << "No message 2\n"<<now;
-						} else {
-							perror("mq_receive()");
-						}
-					} else {
-						//cout << "Received message 2: `" << buffer << "'.\n";
-						parent = atof(buffer);
-						now=(now+parent)/2;
-						sprintf(buffer,"%f",now);
-						cout<<"process "<<pid<<": current temperature"<<now<<endl;
-						if (mq_send(mqself, buffer, strlen(buffer), 0) == -1) {
-							perror("mq_send()");
-						}
-						if (mq_send(mqself, buffer, strlen(buffer), 0) == -1) {
-							perror("mq_send()");
-						}
-						sleep(0.5);
-					}
 				}
 				break;
 			case 2:	
-				mqp = mq_open("/70",O_RDONLY | O_CREAT | O_NONBLOCK,0666,NULL);
+				mqp = mq_open("/70",O_WRONLY | O_CREAT | O_NONBLOCK,0666,NULL);
 				if(mqp == -1){
 					perror("mq_open()");
 				}	
-				mq1 = mq_open("/75",O_RDONLY | O_CREAT | O_NONBLOCK,0666,NULL);
+				mq1 = mq_open("/75",O_WRONLY | O_CREAT | O_NONBLOCK,0666,NULL);
 				if(mq1 == -1){
 					perror("mq_open()");
 				}
-				mq2 = mq_open("/76",O_RDONLY | O_CREAT | O_NONBLOCK,0666,NULL);
+				mq2 = mq_open("/76",O_WRONLY | O_CREAT | O_NONBLOCK,0666,NULL);
 				if(mq2 == -1){
 					perror("mq_open()");
 				}
-				mqself = mq_open("/72",O_WRONLY | O_CREAT | O_NONBLOCK,0666,NULL);
+				mqself = mq_open("/72",O_RDONLY | O_CREAT | O_NONBLOCK,0666,NULL);
 				if(mq2 == -1){
 					perror("mq_open()");
 				}
@@ -226,79 +177,49 @@ while(c1<c2){
 				sprintf(buffer,"%f",now);
 				//cout<<"process "<<pid<<": current temperature"<<now<<endl;	
 				while(true){
-					if (mq_receive(mq1, buffer, bufsize, NULL) == -1) {
+					if (mq_receive(mqself, buffer, bufsize, NULL) == -1) {
 						if (errno == EAGAIN) {
-							//cout << "No message 1\n";
+//							std::cout << "No message\n";
 						} else {
 							perror("mq_receive()");
 						}
 					} else {
-						//cout << "Received message 1: `" << buffer << "'.\n";
-						child1 = atof(buffer);
-c1++;
+						where=buffer[0]-48;
+						strncpy(buffer, buffer+1, bufsize-1);
+						if(where==0){
+							parent=atof(buffer);
+							now=(parent+now)/2;
+							sprintf(buffer,"%d%f",0,now);
+							cout<<"process "<<pid<<": current temperature"<<now<<endl;
+							if (mq_send(mq1, buffer, strlen(buffer), 0) == -1) {
+								perror("mq_send()");
+							}
+							if (mq_send(mq2, buffer, strlen(buffer), 0) == -1) {
+								perror("mq_send()");
+							}}
+						if(where==1){
+							child1=atof(buffer);
+						}
+						if(where==2){
+							child2=atof(buffer);
+							now=(child1+child2+now)/3;	
+							sprintf(buffer,"%d%f",2,now);
+							cout<<"process "<<pid<<": current temperature"<<now<<endl;
+							if (mq_send(mqp, buffer, strlen(buffer), 0) == -1) {
+								perror("mq_send()");
+							}
+
+						}											
 					}
 
-					if (mq_receive(mq2, buffer, bufsize, NULL) == -1) {
-						if (errno == EAGAIN) {
-							//cout << "No message 2\n"<<now;
-						} else {
-							perror("mq_receive()");
-						}
-					} else {
-						//cout << "Received message 2: `" << buffer << "'.\n";
-						child2 = atof(buffer);
-						c2++;
-while(c1<c2){
-
-					if (mq_receive(mq1, buffer, bufsize, NULL) == -1) {
-						if (errno == EAGAIN) {
-							//							cout << "No message 1\n";
-						} else {
-							perror("mq_receive()");
-						}
-					} else {
-						//						cout << "Received message 1: `" << buffer << "'.\n";
-						child1 = atof(buffer);
-						c1++;
-
-					}
-}
-						if(child1==child1) sleep(0.5);
-						now=(now+child1+child2)/3;
-						sprintf(buffer,"%f",now);
-						cout<<"process "<<pid<<": current temperature"<<now<<endl;
-						if (mq_send(mqself, buffer, strlen(buffer), 0) == -1) {
-							perror("mq_send()");
-						}
-					}
-					if (mq_receive(mqp, buffer, bufsize, NULL) == -1) {
-						if (errno == EAGAIN) {
-							//cout << "No message 2\n"<<now;
-						} else {
-							perror("mq_receive()");
-						}
-					} else {
-						//cout << "Received message 2: `" << buffer << "'.\n";
-						parent = atof(buffer);
-						now=(now+parent)/2;
-						sprintf(buffer,"%f",now);
-						cout<<"process "<<pid<<": current temperature"<<now<<endl;
-						if (mq_send(mqself, buffer, strlen(buffer), 0) == -1) {
-							perror("mq_send()");
-						}
-						if (mq_send(mqself, buffer, strlen(buffer), 0) == -1) {
-							perror("mq_send()");
-						}
-						sleep(0.5);
-					}
 				}
 				break;
 			case 3:	
-				mqp = mq_open("/71",O_RDONLY | O_CREAT | O_NONBLOCK,0666,NULL);
+				mqp = mq_open("/71",O_WRONLY | O_CREAT | O_NONBLOCK,0666,NULL);
 				if(mqp == -1){
 					perror("mq_open()");
 				}	
-				mqself = mq_open("/73",O_WRONLY | O_CREAT | O_NONBLOCK,0666,NULL);
+				mqself = mq_open("/73",O_RDONLY | O_CREAT | O_NONBLOCK,0666,NULL);
 				if(mq2 == -1){
 					perror("mq_open()");
 				}
@@ -306,31 +227,35 @@ while(c1<c2){
 				sprintf(buffer,"%f",now);
 				//cout<<"process "<<pid<<": current temperature"<<now<<endl;	
 				while(true){
-					if (mq_receive(mqp, buffer, bufsize, NULL) == -1) {
+					if (mq_receive(mqself, buffer, bufsize, NULL) == -1) {
 						if (errno == EAGAIN) {
-							//cout << "No message 2\n"<<now;
+//							std::cout << "No message\n";
 						} else {
 							perror("mq_receive()");
 						}
 					} else {
-						//cout << "Received message 2: `" << buffer << "'.\n";
-						parent = atof(buffer);
-						now=(now+parent)/2;
-						sprintf(buffer,"%f",now);
-						cout<<"process "<<pid<<": current temperature"<<now<<endl;
-						if (mq_send(mqself, buffer, strlen(buffer), 0) == -1) {
-							perror("mq_send()");
+						where=buffer[0]-48;
+						strncpy(buffer, buffer+1, bufsize-1);
+						if(where==0){
+							parent=atof(buffer);
+							now=(parent+now)/2;
+							sprintf(buffer,"%d%f",1,now);
+							cout<<"process "<<pid<<": current temperature"<<now<<endl;
+							if (mq_send(mqp, buffer, strlen(buffer), 0) == -1) {
+								perror("mq_send()");
+							}
 						}
-						sleep(0.5);
-					}
+
+					}											
 				}
+
 				break;
 			case 4:	
-				mqp = mq_open("/71",O_RDONLY | O_CREAT | O_NONBLOCK,0666,NULL);
+				mqp = mq_open("/71",O_WRONLY | O_CREAT | O_NONBLOCK,0666,NULL);
 				if(mqp == -1){
 					perror("mq_open()");
 				}	
-				mqself = mq_open("/74",O_WRONLY | O_CREAT | O_NONBLOCK,0666,NULL);
+				mqself = mq_open("/74",O_RDONLY | O_CREAT | O_NONBLOCK,0666,NULL);
 				if(mq2 == -1){
 					perror("mq_open()");
 				}
@@ -338,31 +263,35 @@ while(c1<c2){
 				sprintf(buffer,"%f",now);
 				//cout<<"process "<<pid<<": current temperature"<<now<<endl;	
 				while(true){
-					if (mq_receive(mqp, buffer, bufsize, NULL) == -1) {
+					if (mq_receive(mqself, buffer, bufsize, NULL) == -1) {
 						if (errno == EAGAIN) {
-							//cout << "No message 2\n"<<now;
+//							std::cout << "No message\n";
 						} else {
 							perror("mq_receive()");
 						}
 					} else {
-						//cout << "Received message 2: `" << buffer << "'.\n";
-						parent = atof(buffer);
-						now=(now+parent)/2;
-						sprintf(buffer,"%f",now);
-						cout<<"process "<<pid<<": current temperature"<<now<<endl;
-						if (mq_send(mqself, buffer, strlen(buffer), 0) == -1) {
-							perror("mq_send()");
+						where=buffer[0]-48;
+						strncpy(buffer, buffer+1, bufsize-1);
+						if(where==0){
+							parent=atof(buffer);
+							now=(parent+now)/2;
+							sprintf(buffer,"%d%f",2,now);
+							cout<<"process "<<pid<<": current temperature"<<now<<endl;
+							if (mq_send(mqp, buffer, strlen(buffer), 0) == -1) {
+								perror("mq_send()");
+							}
 						}
-						sleep(0.5);
-					}
-				}
+
+					}											
+				
+}
 				break;
 			case 5:	
-				mqp = mq_open("/72",O_RDONLY | O_CREAT | O_NONBLOCK,0666,NULL);
+				mqp = mq_open("/72",O_WRONLY | O_CREAT | O_NONBLOCK,0666,NULL);
 				if(mqp == -1){
 					perror("mq_open()");
 				}	
-				mqself = mq_open("/75",O_WRONLY | O_CREAT | O_NONBLOCK,0666,NULL);
+				mqself = mq_open("/75",O_RDONLY | O_CREAT | O_NONBLOCK,0666,NULL);
 				if(mq2 == -1){
 					perror("mq_open()");
 				}
@@ -370,31 +299,34 @@ while(c1<c2){
 				sprintf(buffer,"%f",now);
 				//cout<<"process "<<pid<<": current temperature"<<now<<endl;	
 				while(true){
-					if (mq_receive(mqp, buffer, bufsize, NULL) == -1) {
+					if (mq_receive(mqself, buffer, bufsize, NULL) == -1) {
 						if (errno == EAGAIN) {
-							//cout << "No message 2\n"<<now;
+//							std::cout << "No message\n";
 						} else {
 							perror("mq_receive()");
 						}
 					} else {
-						//cout << "Received message 2: `" << buffer << "'.\n";
-						parent = atof(buffer);
-						now=(now+parent)/2;
-						sprintf(buffer,"%f",now);
-						cout<<"process "<<pid<<": current temperature"<<now<<endl;
-						if (mq_send(mqself, buffer, strlen(buffer), 0) == -1) {
-							perror("mq_send()");
+						where=buffer[0]-48;
+						strncpy(buffer, buffer+1, bufsize-1);
+						if(where==0){
+							parent=atof(buffer);
+							now=(parent+now)/2;
+							sprintf(buffer,"%d%f",1,now);
+							cout<<"process "<<pid<<": current temperature"<<now<<endl;
+							if (mq_send(mqp, buffer, strlen(buffer), 0) == -1) {
+								perror("mq_send()");
+							}
 						}
-						sleep(0.5);
-					}
+
+					}											
 				}
 				break;
 			case 6:	
-				mqp = mq_open("/72",O_RDONLY | O_CREAT | O_NONBLOCK,0666,NULL);
+				mqp = mq_open("/72",O_WRONLY | O_CREAT | O_NONBLOCK,0666,NULL);
 				if(mqp == -1){
 					perror("mq_open()");
 				}	
-				mqself = mq_open("/76",O_WRONLY | O_CREAT | O_NONBLOCK,0666,NULL);
+				mqself = mq_open("/76",O_RDONLY | O_CREAT | O_NONBLOCK,0666,NULL);
 				if(mq2 == -1){
 					perror("mq_open()");
 				}
@@ -402,24 +334,26 @@ while(c1<c2){
 				sprintf(buffer,"%f",now);
 				//cout<<"process "<<pid<<": current temperature"<<now<<endl;	
 				while(true){
-					if (mq_receive(mqp, buffer, bufsize, NULL) == -1) {
+					if (mq_receive(mqself, buffer, bufsize, NULL) == -1) {
 						if (errno == EAGAIN) {
-							//cout << "No message 2\n"<<now;
+//							std::cout << "No message\n";
 						} else {
 							perror("mq_receive()");
 						}
 					} else {
-						//cout << "Received message 2: `" << buffer << "'.\n";
-						parent = atof(buffer);
-						now=(now+parent)/2;
-						sprintf(buffer,"%f",now);
-						cout<<"process "<<pid<<": current temperature"<<now<<endl;
-						if (mq_send(mqself, buffer, strlen(buffer), 0) == -1) {
-							perror("mq_send()");
+						where=buffer[0]-48;
+						strncpy(buffer, buffer+1, bufsize-1);
+						if(where==0){
+							parent=atof(buffer);
+							now=(parent+now)/2;
+							sprintf(buffer,"%d%f",2,now);
+							cout<<"process "<<pid<<": current temperature"<<now<<endl;
+							if (mq_send(mqp, buffer, strlen(buffer), 0) == -1) {
+								perror("mq_send()");
+							}
 						}
-						sleep(0.5);
-					}
-					//					cout<<"in";
+
+					}											
 				}
 				break;
 
